ba99527e1e482204c310bb24bbd0fb5c8eb58023
etil2jz <81570777+etil2jz@users.noreply.github.com>
2024-12-29 22:06:57 +0100

    Initial Fabric example mod

---
+++ src/client/java/ovh/quic/simdeverything/SIMDEverythingClient.java
@@ -0,0 +1,10 @@
+package ovh.quic.simdeverything;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SIMDEverythingClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
