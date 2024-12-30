861242bd06789e48c020256e5946479a0c0d8b01
etil2jz <81570777+etil2jz@users.noreply.github.com>
2024-12-30 13:34:21 +0100

    Fix crash
    Collisions and movements are buggy right now. The opt is clearly not usable as it. I should take a look at the VoxelShape class.

--- src/main/java/ovh/quic/simdeverything/mixin/SIMDVec3Mixin.java
+++ src/main/java/ovh/quic/simdeverything/mixin/SIMDVec3Mixin.java
@@ -11,55 +11,96 @@ import jdk.incubator.vector.*;
 
 @Mixin(Vec3.class)
 public class SIMDVec3Mixin {
-
 	@Shadow @Final public double x;
 	@Shadow @Final public double y;
 	@Shadow @Final public double z;
 
 	private static final VectorSpecies<Double> SPECIES = DoubleVector.SPECIES_PREFERRED;
+	private static final ThreadLocal<double[]> VECTOR_BUFFER = ThreadLocal.withInitial(() -> new double[SPECIES.length() * 2]);
 
 	@Overwrite
 	public Vec3 add(double d, double e, double f) {
-		DoubleVector v1 = DoubleVector.fromArray(SPECIES, new double[] { this.x, this.y, this.z }, 0);
-		DoubleVector v2 = DoubleVector.broadcast(SPECIES, d);
-		v2 = v2.withLane(1, e);
-		v2 = v2.withLane(2, f);
+		double[] buffer = VECTOR_BUFFER.get();
+		buffer[0] = this.x;
+		buffer[1] = this.y;
+		buffer[2] = this.z;
+
+		buffer[3] = d;
+		buffer[4] = e;
+		buffer[5] = f;
+
+		DoubleVector v1 = DoubleVector.fromArray(SPECIES, buffer, 0);
+		DoubleVector v2 = DoubleVector.fromArray(SPECIES, buffer, 3);
 		DoubleVector result = v1.add(v2);
+
 		return new Vec3(result.lane(0), result.lane(1), result.lane(2));
 	}
 
 	@Overwrite
 	public Vec3 subtract(double d, double e, double f) {
-		DoubleVector v1 = DoubleVector.fromArray(SPECIES, new double[] { this.x, this.y, this.z }, 0);
-		DoubleVector v2 = DoubleVector.broadcast(SPECIES, d);
-		v2 = v2.withLane(1, e);
-		v2 = v2.withLane(2, f);
+		double[] buffer = VECTOR_BUFFER.get();
+		buffer[0] = this.x;
+		buffer[1] = this.y;
+		buffer[2] = this.z;
+
+		buffer[3] = d;
+		buffer[4] = e;
+		buffer[5] = f;
+
+		DoubleVector v1 = DoubleVector.fromArray(SPECIES, buffer, 0);
+		DoubleVector v2 = DoubleVector.fromArray(SPECIES, buffer, 3);
 		DoubleVector result = v1.sub(v2);
+
 		return new Vec3(result.lane(0), result.lane(1), result.lane(2));
 	}
 
 	@Overwrite
 	public Vec3 scale(double d) {
-		DoubleVector v1 = DoubleVector.fromArray(SPECIES, new double[] { this.x, this.y, this.z }, 0);
+		double[] buffer = VECTOR_BUFFER.get();
+		buffer[0] = this.x;
+		buffer[1] = this.y;
+		buffer[2] = this.z;
+
+		DoubleVector v1 = DoubleVector.fromArray(SPECIES, buffer, 0);
 		DoubleVector result = v1.mul(d);
+
 		return new Vec3(result.lane(0), result.lane(1), result.lane(2));
 	}
 
 	@Overwrite
 	public Vec3 multiply(Vec3 vec3) {
-		DoubleVector v1 = DoubleVector.fromArray(SPECIES, new double[] { this.x, this.y, this.z }, 0);
-		DoubleVector v2 = DoubleVector.fromArray(SPECIES, new double[] { vec3.x, vec3.y, vec3.z }, 0);
+		double[] buffer = VECTOR_BUFFER.get();
+		buffer[0] = this.x;
+		buffer[1] = this.y;
+		buffer[2] = this.z;
+
+		buffer[3] = vec3.x;
+		buffer[4] = vec3.y;
+		buffer[5] = vec3.z;
+
+		DoubleVector v1 = DoubleVector.fromArray(SPECIES, buffer, 0);
+		DoubleVector v2 = DoubleVector.fromArray(SPECIES, buffer, 3);
 		DoubleVector result = v1.mul(v2);
+
 		return new Vec3(result.lane(0), result.lane(1), result.lane(2));
 	}
 
 	@Overwrite
 	public double distanceToSqr(Vec3 vec3) {
-		DoubleVector v1 = DoubleVector.fromArray(SPECIES, new double[] { this.x, this.y, this.z }, 0);
-		DoubleVector v2 = DoubleVector.fromArray(SPECIES, new double[] { vec3.x, vec3.y, vec3.z }, 0);
-		DoubleVector delta = v1.sub(v2);
-		double sumOfSquares = delta.mul(delta).reduceLanes(VectorOperators.ADD);
-		return sumOfSquares;
-	}
+		double[] buffer = VECTOR_BUFFER.get();
+		buffer[0] = this.x;
+		buffer[1] = this.y;
+		buffer[2] = this.z;
 
+		buffer[3] = vec3.x;
+		buffer[4] = vec3.y;
+		buffer[5] = vec3.z;
+
+		DoubleVector v1 = DoubleVector.fromArray(SPECIES, buffer, 0);
+		DoubleVector v2 = DoubleVector.fromArray(SPECIES, buffer, 3);
+
+		DoubleVector delta = v1.sub(v2);
+
+		return (double)delta.mul(delta).reduceLanes(VectorOperators.ADD);
+	}
 }
